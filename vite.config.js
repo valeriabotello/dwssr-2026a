3174b6c45090d48cbd6a85820101e1777d680621
valery.BO <valeria.botello.olvera2410@gmail.com>
2026-03-25 16:55:56 +0000

    🔧chore:vite configuracion en progreso

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+//importamos la funcion de configuración de Vite
+import { defineConfig } from 'vite'; 
+//importamos un resolvedor de rutas para poder usar alias en las rutas de importación
+import { resolve } from 'node:path';
+//exportamos la instancia de configuración de Vite
+export default defineConfig({})
