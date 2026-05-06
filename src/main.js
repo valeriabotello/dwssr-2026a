ac57fd787c99e8080b576edf15983d69c945a52c
valery.BO <valeria.botello.olvera2410@gmail.com>
2026-05-06 15:47:18 +0000

    🌟feat:Prettier format JavaScript and add Prettier configuration

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ import './styles/main.css';
 import './styles/header.css';
 
 // Scripts principales
-console.log("🚀 Vite + Express + Handlebars");
+console.log('🚀 Vite + Express + Handlebars funcionando');
+
+let students = [
+  {
+    name: 'Johny',
+    surname: 'Walker',
+  },
+  {
+    
+    name: 'Sue',
+    surname: 'Storm',
+  },
+];
+
+console.log(students);
